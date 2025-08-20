feead742ee5d5458a5d6ed184f4ccf9cdf2841dc
skinnyK <skinnykdev@gmail.com>
2025-08-20 10:26:38 +0200

    add base repository value to config

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,11 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import tailwindcss from '@tailwindcss/vite';
 
+const repository = '/project_exam2/';
+
 // https://vite.dev/config/
 export default defineConfig({
+  base: repository,
   plugins: [react(), tailwindcss()],
   test: {
         environment: 'jsdom',
