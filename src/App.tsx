fc8fbc1e2db42b1e1efe52a329a9ab656ad44db5
skinnyK <skinnykdev@gmail.com>
2025-08-20 11:46:05 +0200

    more fixing on the 404 page hahahahahahaha (manic laughter)

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 function App() {
   return (
-    <h1 className='mt-8 text-center text-3xl font-semibold text-red-900'>
+    <h1 className='mt-8 text-center text-3xl font-semibold text-green-900'>
       Project exam 2
     </h1>
   );
