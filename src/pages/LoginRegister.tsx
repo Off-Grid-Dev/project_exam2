e3f6945e1bc5809ec5137c476e422c04006b5f04
skinnyK <skinnykdev@gmail.com>
2025-08-22 15:33:21 +0200

    added LoginRegister (/welcome) page to house the login and register form components

--- src/pages/LoginRegister.tsx
+++ src/pages/LoginRegister.tsx
@@ -1,3 +1,31 @@
+import { useState } from 'react';
+import { LoginForm } from '../components/Login';
+import { RegisterForm } from '../components/Register';
+
 export const LoginRegister = () => {
-  return <h1>LOGIN OR REGISTER</h1>;
+  const [isLoginOrRegister, setIsLoginOrRegister] = useState<
+    undefined | 'login' | 'register'
+  >(undefined);
+
+  return (
+    <div className='m-auto mx-auto mt-8 grid w-fit place-content-center gap-4 rounded-lg border-2 border-amber-700 p-4 text-center'>
+      <h1>LOGIN OR REGISTER</h1>
+      <div className='my-4 flex justify-center gap-4'>
+        <button
+          onClick={() => setIsLoginOrRegister('login')}
+          className='cursor-pointer rounded-lg border border-amber-400 px-3 py-2 hover:border-amber-950'
+        >
+          Log In
+        </button>
+        <button
+          onClick={() => setIsLoginOrRegister('register')}
+          className='cursor-pointer rounded-lg border border-amber-400 px-3 py-2 hover:border-amber-950'
+        >
+          Register
+        </button>
+      </div>
+      {isLoginOrRegister === 'login' && <LoginForm />}
+      {isLoginOrRegister === 'register' && <RegisterForm />}
+    </div>
+  );
 };
