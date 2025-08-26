a0354ca8ce145935e95689276df0353d3faa7a7f
skinnyK <skinnykdev@gmail.com>
2025-08-26 16:14:05 +0200

    created Authorization context to wrap around page

---
+++ src/api/AuthContext.tsx
@@ -0,0 +1,36 @@
+import { createContext, useContext, useState, type ReactNode } from 'react';
+
+type AuthProviderProps = {
+  children: ReactNode;
+};
+
+type AuthContextProps = {
+  isLoggedIn: boolean;
+  login: () => void;
+  logout: () => void;
+};
+
+const AuthContext = createContext<AuthContextProps | undefined>(undefined);
+
+export const AuthProvider = ({ children }: AuthProviderProps) => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const login = () => setIsLoggedIn(true);
+  const logout = () => setIsLoggedIn(false);
+
+  return (
+    <AuthContext.Provider value={{ isLoggedIn, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export const useAuth = () => {
+  const context = useContext(AuthContext);
+
+  if (context === undefined) {
+    throw new Error('useAuth must be used within an AuthProvider');
+  }
+
+  return context;
+};
