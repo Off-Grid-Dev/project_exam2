c804d0076c4184a31d41ebd0eafdff651bed4851
skinnyK <skinnykdev@gmail.com>
2025-08-26 16:12:42 +0200

    abstracted AppRoutes to be able to use useAuth hook to designate visibility of welcome route

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,30 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Home } from './pages/Home';
 import { LoginRegister } from './pages/LoginRegister';
 import { Wrapper } from './components/layout/Wrapper';
+import { AuthProvider, useAuth } from './api/AuthContext';
+
+const AppRoutes = () => {
+  const { isLoggedIn } = useAuth();
+  return (
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route
+        path='/welcome'
+        element={!isLoggedIn ? <LoginRegister /> : <Home />}
+      />
+      <Route path='*' element={<Home />} />
+    </Routes>
+  );
+};
 
 function App() {
   return (
     <BrowserRouter basename='/project_exam2'>
-      <Wrapper>
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/welcome' element={<LoginRegister />} />
-          <Route path='*' element={<Home />} />
-        </Routes>
-      </Wrapper>
+      <AuthProvider>
+        <Wrapper>
+          <AppRoutes />
+        </Wrapper>
+      </AuthProvider>
     </BrowserRouter>
   );
 }
