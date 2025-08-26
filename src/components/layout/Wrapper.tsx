821f17eb2d6b87ecc93d476e02f9004b2e6b14de
skinnyK <skinnykdev@gmail.com>
2025-08-26 16:15:34 +0200

    useAuth and useNavigate implemented in wrapper logout function

--- src/components/layout/Wrapper.tsx
+++ src/components/layout/Wrapper.tsx
@@ -1,11 +1,23 @@
 import type { ReactNode, FC } from 'react';
 import { Link } from 'react-router-dom';
+import { useAuth } from '../../api/AuthContext';
+import { getData } from '../../api/api';
+import { useNavigate } from 'react-router-dom';
 
 type WrapperProps = {
   children: ReactNode;
 };
 
 export const Wrapper: FC<WrapperProps> = ({ children }) => {
+  const { isLoggedIn, logout } = useAuth();
+  const navigate = useNavigate();
+
+  async function handleLogout() {
+    logout();
+    console.log('user is logged out.');
+    await getData('logout user');
+    navigate('/');
+  }
   return (
     <>
       <header className='flex justify-between bg-amber-950 px-8 py-6 align-middle text-white'>
@@ -19,14 +31,26 @@ export const Wrapper: FC<WrapperProps> = ({ children }) => {
               home
             </Link>
           </li>
-          <li>
-            <Link
-              className='rounded-lg bg-white px-3 py-2 font-semibold text-amber-950 transition-colors duration-200 hover:bg-amber-800 hover:text-white'
-              to={'/welcome'}
-            >
-              welcome
-            </Link>
-          </li>
+          {!isLoggedIn && (
+            <li>
+              <Link
+                className='rounded-lg bg-white px-3 py-2 font-semibold text-amber-950 transition-colors duration-200 hover:bg-amber-800 hover:text-white'
+                to={'/welcome'}
+              >
+                welcome
+              </Link>
+            </li>
+          )}
+          {isLoggedIn && (
+            <li>
+              <button
+                className='rounded-lg bg-white px-3 py-2 font-semibold text-amber-950 transition-colors duration-200 hover:bg-amber-800 hover:text-white'
+                onClick={handleLogout}
+              >
+                logout
+              </button>
+            </li>
+          )}
         </ul>
       </header>
       {children}
