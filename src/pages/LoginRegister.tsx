18829fa94f291208b5b3eba16b42ee761da64aac
skinnyK <skinnykdev@gmail.com>
2025-08-26 16:16:04 +0200

    only show login or register options when one of the two is not chosen

--- src/pages/LoginRegister.tsx
+++ src/pages/LoginRegister.tsx
@@ -9,21 +9,25 @@ export const LoginRegister = () => {
 
   return (
     <div className='m-auto mx-auto mt-8 grid w-fit place-content-center gap-4 rounded-lg border-2 border-amber-700 p-4 text-center'>
-      <h1>LOGIN OR REGISTER</h1>
-      <div className='my-4 flex justify-center gap-4'>
-        <button
-          onClick={() => setIsLoginOrRegister('login')}
-          className='cursor-pointer rounded-lg border border-amber-400 px-3 py-2 hover:border-amber-950'
-        >
-          Log In
-        </button>
-        <button
-          onClick={() => setIsLoginOrRegister('register')}
-          className='cursor-pointer rounded-lg border border-amber-400 px-3 py-2 hover:border-amber-950'
-        >
-          Register
-        </button>
-      </div>
+      {isLoginOrRegister === undefined && (
+        <>
+          <h1>LOGIN OR REGISTER</h1>
+          <div className='my-4 flex justify-center gap-4'>
+            <button
+              onClick={() => setIsLoginOrRegister('login')}
+              className='cursor-pointer rounded-lg border border-amber-400 px-3 py-2 hover:border-amber-950'
+            >
+              Log In
+            </button>
+            <button
+              onClick={() => setIsLoginOrRegister('register')}
+              className='cursor-pointer rounded-lg border border-amber-400 px-3 py-2 hover:border-amber-950'
+            >
+              Register
+            </button>
+          </div>
+        </>
+      )}
       {isLoginOrRegister === 'login' && <LoginForm />}
       {isLoginOrRegister === 'register' && <RegisterForm />}
     </div>
