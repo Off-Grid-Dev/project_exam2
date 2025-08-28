7d26a5082869138477fdffa2354f7027a62340e0
skinnyK <skinnykdev@gmail.com>
2025-08-28 16:36:26 +0200

    - made sure even single venue returns are treated as arrays for simplicity and mapping purposes

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,11 @@
-import { useState, type ChangeEvent } from 'react';
+import { useLayoutEffect, useState, type ChangeEvent } from 'react';
 import { getData } from '../api/api.ts';
+import { VenuesList } from '../components/venues/VenueList.tsx';
+import { type Venue } from '../types/api/venue.ts';
+import type { VenuesResponse } from '../types/api/responses.ts';
 
 export const Home = () => {
+  const [venues, setVenues] = useState<Venue[]>([]);
   const [venueID, setVenueID] = useState<string | undefined>(undefined);
   const [venueQuery, setVenueQuery] = useState<string | undefined>(undefined);
 
@@ -13,45 +17,56 @@ export const Home = () => {
     setVenueQuery(e.target.value.trim());
   }
 
+  async function normalizeVenueReturn() {
+    const res = (await getData('get venues')) as VenuesResponse;
+    if (!res) {
+      setVenues([]);
+      return;
+    }
+    const data = res.data;
+    setVenues(Array.isArray(data) ? data : [data]);
+  }
+
+  useLayoutEffect(() => {
+    normalizeVenueReturn();
+  }, []);
+
   return (
-    <div className='grid min-h-screen place-content-center gap-4'>
-      <h1 className='mt-8 text-center text-3xl font-semibold text-green-900'>
-        Project exam 2
-      </h1>
-      <button
-        className='cursor-pointer rounded-2xl bg-amber-800 px-3 py-2 hover:bg-amber-700'
-        onClick={() => getData('get venues')}
-      >
-        Fetch Venues
-      </button>
-      <input
-        type='text'
-        name='venueID'
-        id='venueID'
-        value={venueID}
-        onChange={handleIDUpdate}
-        className='border border-black p-2'
-      />
-      <button
-        className='cursor-pointer rounded-2xl bg-green-800 px-3 py-2 hover:bg-green-700'
-        onClick={() => getData('venue by id', { id: venueID })}
-      >
-        Fetch Venue By ID
-      </button>
-      <input
-        type='text'
-        name='venueQuery'
-        id='venueQuery'
-        value={venueQuery}
-        onChange={handleVenueSearch}
-        className='border border-black p-2'
-      />
-      <button
-        className='cursor-pointer rounded-2xl bg-blue-800 px-3 py-2 hover:bg-blue-700'
-        onClick={() => getData('venue by search', { q: venueQuery })}
-      >
-        Search for Venues
-      </button>
-    </div>
+    <>
+      <div className='grid min-h-screen place-content-center gap-4'>
+        <h1 className='mt-8 text-center text-3xl font-semibold text-green-900'>
+          Project exam 2
+        </h1>
+        <input
+          type='text'
+          name='venueID'
+          id='venueID'
+          value={venueID}
+          onChange={handleIDUpdate}
+          className='border border-black p-2'
+        />
+        <button
+          className='cursor-pointer rounded-2xl bg-green-800 px-3 py-2 hover:bg-green-700'
+          onClick={() => getData('venue by id', { id: venueID })}
+        >
+          Fetch Venue By ID
+        </button>
+        <input
+          type='text'
+          name='venueQuery'
+          id='venueQuery'
+          value={venueQuery}
+          onChange={handleVenueSearch}
+          className='border border-black p-2'
+        />
+        <button
+          className='cursor-pointer rounded-2xl bg-blue-800 px-3 py-2 hover:bg-blue-700'
+          onClick={() => getData('venue by search', { q: venueQuery })}
+        >
+          Search for Venues
+        </button>
+      </div>
+      <VenuesList venues={venues} />
+    </>
   );
 };
