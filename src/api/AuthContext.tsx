3fad3f769a5e9896835778dc097323ee7c1ddbdb
skinnyK <skinnykdev@gmail.com>
2025-08-31 11:35:17 +0200

    improved error message to explain more clearly what is happening with the AuthContext component

--- src/api/AuthContext.tsx
+++ src/api/AuthContext.tsx
@@ -29,7 +29,9 @@ export const useAuth = () => {
   const context = useContext(AuthContext);
 
   if (context === undefined) {
-    throw new Error('useAuth must be used within an AuthProvider');
+    throw new Error(
+      `useAuth must be used within an AuthProvider\n (that means you have to wrap the component with the AuthProvider component)`,
+    );
   }
 
   return context;
