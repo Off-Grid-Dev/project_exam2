2bc2c1bfb66c8f14816faee2eff07681d06e18b4
skinnyK <skinnykdev@gmail.com>
2025-09-02 12:34:44 +0200

    typo fixed

--- src/api/venues/getVenues.ts
+++ src/api/venues/getVenues.ts
@@ -19,7 +19,7 @@ export const getVenues = async (
   if (limit) query.append('limit', limit.toString());
   if (page) query.append('page', page.toString());
   if (_owner) query.append('_owner', _owner.toString());
-  if (_bookings) query.append('_count', _bookings.toString());
+  if (_bookings) query.append('_bookings', _bookings.toString());
 
   const url = query.toString()
     ? `${API_VENUES}?${query.toString()}`
