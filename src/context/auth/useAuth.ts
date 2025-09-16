f6e1b8589613cf4508fec33f641b71ef7fd76f34
skinnyK <skinnykdev@gmail.com>
2025-09-16 19:09:32 +0200

    changed some syntax

--- src/context/auth/useAuth.ts
+++ src/context/auth/useAuth.ts
@@ -2,13 +2,12 @@ import { useContext } from 'react';
 import { AuthContext } from './AuthContext';
 
 export const useAuth = () => {
-  const context = useContext(AuthContext);
-
-  if (context === undefined) {
+  const ctx = useContext(AuthContext);
+  if (!ctx) {
     throw new Error(
       `useAuth must be used within an AuthProvider\n (that means you have to wrap the component with the AuthProvider component)`,
     );
   }
 
-  return context;
+  return ctx;
 };
