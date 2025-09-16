92022b3aee943371e6f2d0d248c48c58bf20e6ff
skinnyK <skinnykdev@gmail.com>
2025-09-16 19:09:42 +0200

    fixed syntax

--- src/context/ui/useBreakpoint.ts
+++ src/context/ui/useBreakpoint.ts
@@ -3,7 +3,11 @@ import { BreakpointContext } from './BreakpointContext';
 
 export const useBreakpoint = () => {
   const ctx = useContext(BreakpointContext);
-  if (!ctx)
-    throw new Error('useBreakpoint must be used within BreakpointProvider');
+  if (!ctx) {
+    throw new Error(
+      'useBreakpoint must be used within BreakpointProvider\n (that means you have to wrap the component with the BreakpointProvider component)',
+    );
+  }
+
   return ctx;
 };
