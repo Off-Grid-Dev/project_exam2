54b5a229da05ee39b865d169c779befc4da51aae
skinnyK <skinnykdev@gmail.com>
2025-09-20 14:38:25 +0200

    fixed linting errors from the github workflow

--- src/pages/Venue.tsx
+++ src/pages/Venue.tsx
@@ -1,34 +1,39 @@
 import { useParams } from 'react-router-dom';
 import { Wrapper } from '../components/layout/Wrapper';
 import { fetchVenues } from '../api/api.ts';
-import { type Venue } from '../types/api/venue.ts';
+import type { Venue as VenueType } from '../types/api/venue.ts';
 import type { VenuesResponse } from '../types/api/responses.ts';
 import { ApiFunctions } from '../api/apiFunctionsEnum.ts';
-import { useCallback, useLayoutEffect, useState } from 'react';
+import { useCallback, useLayoutEffect, useState, type FC } from 'react';
 
-const Venue = () => {
+const Venue: FC<VenueType> = () => {
   const [isLoading, setIsLoading] = useState(false);
   const { id } = useParams();
   const { GetVenueById } = ApiFunctions;
 
-  const fetchVenue = useCallback(async (id: string) => {
-    setIsLoading(true);
-    let res;
-    try {
-      res = (await fetchVenues(GetVenueById, { id })) as
-        | VenuesResponse
-        | undefined;
-    } catch (err) {
-      throw new Error('Could not fetch this venue');
-    } finally {
-      setIsLoading(false);
-      return res;
-    }
-  }, []);
+  const fetchVenue = useCallback(
+    async (id: string) => {
+      setIsLoading(true);
+      try {
+        const res = (await fetchVenues(GetVenueById, { id })) as
+          | VenuesResponse
+          | undefined;
+        return res;
+      } catch (error) {
+        // preserve original error for debugging
+        // eslint-disable-next-line no-console
+        console.error(error);
+        throw new Error('Could not fetch this venue');
+      } finally {
+        setIsLoading(false);
+      }
+    },
+    [GetVenueById],
+  );
 
   useLayoutEffect(() => {
     if (typeof id === 'string') fetchVenue(id);
-  }, []);
+  }, [fetchVenue, id]);
 
   return (
     <Wrapper>
