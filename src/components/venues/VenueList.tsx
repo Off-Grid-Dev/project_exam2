7057e00a33595b250cab6f248b17bbfd1e5a2931
skinnyK <skinnykdev@gmail.com>
2025-09-20 14:27:06 +0200

    set up pagination

--- src/components/venues/VenueList.tsx
+++ src/components/venues/VenueList.tsx
@@ -1,15 +1,27 @@
 import { VenuesCard } from './VenueCard';
 import { type Venue } from '../../types/api/venue';
-import type { FC } from 'react';
+import type { Dispatch, FC, SetStateAction } from 'react';
 import Button from '../Button';
 // import { useBreakpoint } from '../../context/ui/useBreakpoint';
 
 type VenuesListType = {
   venues: Venue[];
   isLoading?: boolean;
+  page: number;
+  setPage: Dispatch<SetStateAction<number>>;
+  pagination: {
+    isFirstPage: boolean | undefined;
+    isLastPage: boolean | undefined;
+  };
 };
 
-export const VenuesList: FC<VenuesListType> = ({ venues, isLoading }) => {
+export const VenuesList: FC<VenuesListType> = ({
+  venues,
+  isLoading,
+  page,
+  setPage,
+  pagination,
+}) => {
   if ((!venues || venues.length === 0) && !isLoading) {
     return <p className='text-center'>No venues to show</p>;
   }
@@ -18,18 +30,35 @@ export const VenuesList: FC<VenuesListType> = ({ venues, isLoading }) => {
     return null;
   }
 
-  function handlePreviousPage() {}
-  function handleNextPage() {}
+  function handlePreviousPage() {
+    if (page > 1) {
+      setPage((prev) => prev - 1);
+    } else {
+      return;
+    }
+  }
+  function handleNextPage() {
+    setPage((prev) => prev + 1);
+  }
 
   return (
     <>
-      <div className='mx-auto mt-3 flex gap-3'>
+      <div className='mx-auto mt-3 grid grid-cols-[1fr_auto_1fr] gap-3'>
         <Button
           label='previous page'
           type='button'
-          onclick={handlePreviousPage}
+          onClick={handlePreviousPage}
+          disabled={Boolean(pagination.isFirstPage)}
         />
-        <Button label='next page' type='button' onclick={handleNextPage} />
+        <span className='bg-bg-med grid aspect-square place-content-center rounded-full'>
+          {page}
+        </span>
+        <Button
+          label='next page'
+          type='button'
+          onClick={handleNextPage}
+          disabled={Boolean(pagination.isLastPage)}
+        />
       </div>
       <div className='my-4 grid grid-cols-2 gap-3'>
         {venues.map((venue) => (
